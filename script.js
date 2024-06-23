e3ba30067d94fc5198eadadf9820f17e724be6ef
Roni Kaufman <48394336+ronikaufman@users.noreply.github.com>
2024-06-23 16:21:31 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const verbs = ["buy", "use", "break", "fix", "trash", "change", "mail", "upgrade", "charge", "point", "zoom", "press", "snap", "work", "erase", "write", "cut", "past", "save", "load", "check", "rewrite", "plug", "play", "burn", "rip", "drag and drop", "unzip", "lock", "fill", "call", "find", "view", "code", "jam", "unlock", "surf", "scroll", "pause", "click", "cross", "crack", "switch", "update", "name", "read", "tune", "print", "scan", "send", "fax", "rename", "touch", "bring", "pay", "watch", "turn", "leave", "start", "format"];
+
+function createButtons() {
+    let divEl = document.getElementById("buttons");
+    for (let verb of verbs) {
+        let buttonEl = document.createElement("button");
+        buttonEl.setAttribute("onclick", "speak(\""+verb+" it\")");
+        buttonEl.innerHTML = verb;
+        divEl.appendChild(buttonEl);
+    }
+}
+
+function speak(txt) {
+    const utterance = new SpeechSynthesisUtterance(txt);
+
+    const voices = speechSynthesis.getVoices();
+    utterance.voice = voices[0];
+
+    utterance.pitch = 2;
+    utterance.rate = 1.5;
+    speechSynthesis.speak(utterance);
+}
+
+createButtons();
+let titleEl = document.getElementById("title");
+titleEl.setAttribute("onclick", "speak(\"Technologic\")");
